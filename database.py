b5a0e3f68c960c82cf0ab492cf88e7953c03bc7c
KirillTotkov <kirillatot@gmail.com>
2022-02-12 03:50:31 +0300

    Heroku

---
+++ database.py
@@ -0,0 +1,43 @@
+from faunadb import query
+from faunadb.client import FaunaClient
+import os
+
+fauna_key = os.environ['FAUNAKEY']
+clientf = FaunaClient(fauna_key)
+
+def get_all_cource():
+    result = clientf.query(query.map_(query.lambda_("x", query.get(query.var('x'))),
+                                      query.paginate(query.match(query.index('all_courses')))))
+    result = result['data']
+    data = [cource['data'] for cource in result]
+
+    return data
+
+
+def user_exists(user_id):
+    res = clientf.query(query.map_(query.lambda_("x", query.get(query.var('x'))),
+                                   query.paginate(query.match(query.index('users_by_id'), user_id))))
+    return bool(len(res['data']))
+
+
+def add_user(user_id, user_name):
+    clientf.query(
+        query.create(query.collection('Users'), {'data': {'user_id': user_id, 'user_name': user_name, 'active': 1}}))
+
+
+def set_acive(user_id, acive):
+    clientf.query(
+        query.update(query.select('ref', query.get(query.match(query.index("users_by_id"), user_id))),
+                     {'data': {'active': acive}}))
+
+
+def get_users():
+    res = clientf.query(query.map_(query.lambda_("x", query.get(query.var('x'))),
+                                   query.paginate(query.match(query.index('get_all_users')))))
+
+    data=[x['data'] for x in res['data']]
+
+    for i  in data:
+        del i['user_name']
+
+    return data
---
+++ moodle.py
@@ -0,0 +1,82 @@
+import requests
+from bs4 import BeautifulSoup
+from faunadb import query
+from faunadb.client import FaunaClient
+import os
+from database import get_all_cource
+
+fauna_key = os.environ['FAUNAKEY']
+lmskey = os.environ['LMSKEY']
+clientf = FaunaClient(fauna_key)
+
+
+def get_count_in_lms(id):
+    request = f'https://lms.syktsu.ru/webservice/rest/server.php?wstoken={lmskey}&' \
+              f'wsfunction=core_course_get_contents&courseid={id}&moodlewsrestformat=json'
+
+    result = requests.get(request, headers={"User-Agent": "Mozilla/5.0"})
+    data = result.json()
+    tasks_cource = [x for x in data if len(x['modules']) != 0]
+    tasks_list = []
+    for i in tasks_cource:
+        for j in i.get('modules'):
+            if 'description' not in j.keys():
+                tasks_list.append(
+                    {
+                        'name': j['name'],
+                        'link': j['url']
+                    }
+                )
+            elif j['modname'] == 'label':
+                tasks_list.append(
+                    {
+                        'name': j['name']
+                    }
+                )
+            else:
+                description = j['description']
+                soup = BeautifulSoup(description, 'lxml').text
+                tasks_list.append(
+                    {
+                        'name': j['name'],
+                        'link': j['url'],
+                        'description': soup
+                    }
+                )
+    return tasks_list
+
+
+def collect_data():
+    data = get_all_cource()
+    answer_data = []
+    for cource in data:
+        tasks_list = get_count_in_lms(cource['id'])
+
+        old_count = cource['count']
+        real_count = len(tasks_list)
+        diff = real_count - old_count
+
+        if diff == 0:
+            pass
+            # print("Ничего не изменилось")
+            # print(f"{cource['fullname']} \n" )
+        elif diff > 0:
+            # clientf.query(
+            #     query.update(query.select('ref', query.get(query.match(query.index("courses_by_id"), cource['id']))),
+            #                  {'data': {'count': real_count}}))
+
+            tasks_list = tasks_list[-diff:]
+
+            for i in tasks_list:
+                i['fullname'] = cource['fullname']
+            return tasks_list
+        else:
+            print("ERROR \n")
+
+
+def main():
+    collect_data()
+
+
+if __name__ == "__main__":
+    main()
